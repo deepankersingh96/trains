1f43682f1df90f8331f54ea7624800ff7eb6903b
Singh Deepanker (RBEI/ERD1) <ise3kor@bosch.com>
2020-04-13 10:27:16 +0530

    testing

--- examples/tensorflow_mnist_with_summaries.py
+++ examples/tensorflow_mnist_with_summaries.py
@@ -200,9 +200,9 @@ if __name__ == '__main__':
                         help='If true, uses fake data for unit testing.')
     parser.add_argument('--max_steps', type=int, default=300,
                         help='Number of steps to run trainer.')
-    parser.add_argument('--learning_rate', type=float, default=0.001,
+    parser.add_argument('--learning_rate', type=float, default=0.003,
                         help='Initial learning rate')
-    parser.add_argument('--dropout', type=float, default=0.9,
+    parser.add_argument('--dropout', type=float, default=0.7,
                         help='Keep probability for training dropout.')
     parser.add_argument('--data_dir', type=str,
                         default=os.path.join(tempfile.gettempdir(), 'tensorflow', 'mnist', 'input_data'),
